2b5d2906a92e65c8edd390e16d63c929e3d21ff9
Aryan Atif <aryanatif01@gmail.com>
2026-04-27 15:46:12 +0500

    add the basic main structure to build the program on top of

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <fstream.h>
 
-int main () 
+int main (int agrc, char *agrv) 
 {
+  if (agrc < 3) { printf ("Usage: ./grep [Options] Patterns [FILE]"); return 1;}
+  
+  char *options;
+  char *pattern;
+  char *file;
+  int starting_cmd_index = 0;  /* Assuming that the user has entered not options */
+ 
+  if (argc > 3) /* Or if they have entered an option*/
+  {
+    starting_cmd_index = 1;
+    char *options = argv[1];                      // ./grep [OPTIONS] Pattern [FILE]
+  }                                               //        ^^^^^^^^^
+  pattern = argv[1 + starting_cmd_index];         // ./grep [OPTIONS] Pattern [FILE]
+                                                  //                  ^^^^^^^
+  file = argv[2 + starting_cmd_index];    // ./grep [OPTIONS] Pattern [FILE]
+                                                  //                          ^^^^^^
 
+    
   return 0;
 }
